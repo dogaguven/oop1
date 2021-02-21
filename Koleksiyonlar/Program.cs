82463e1a4c1ae660bd0686e15d38483d637a7929
Doga <dogaguven2000@hotmail.com>
2021-02-21 14:49:55 +0300

    Proje dosyası ekle.

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler2 = new List<string> { "Doğa", "Erva", "Duygu", "Nevzat" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+
+            isimler2.Add("Damla");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[0]);
+        }
+    }
+}
